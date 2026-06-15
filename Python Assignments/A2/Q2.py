eeed27d46e3e375c262b43970054473667836a32
omnaravane <omnaravane@gmail.com>
2026-06-15 18:28:15 +0530

    Update Q2.py

--- Python Assignments/A2/Q2.py
+++ Python Assignments/A2/Q2.py
@@ -1,3 +1,4 @@
+#to show the difference between the two inputs
 a=10
 b=10
 
@@ -8,4 +9,7 @@ a=[10]
 b=[10]
 
 print("address:",id(a))
+print(type(a))
 print("address:",id(b))
+
+# in the first 2 numbers id of both the numbers is same but below the numbers are treated as a list thats why a and b have different memory adress
