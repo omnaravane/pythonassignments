462c37a7ffb96d0fd5e7431162ac6a4602c9a82c
omnaravane <omnaravane@gmail.com>
2026-06-15 18:23:53 +0530

    Update Q6.py

--- Python Assignments/A2/Q6.py
+++ Python Assignments/A2/Q6.py
@@ -1,5 +1,16 @@
+#program to show addition subtracton,multiplication and division
 print("enter first number")
 a=int(input())
 
 print("enter second number")
 b=int(input())
+
+ans1=a+b
+ans2=a-b
+ans3=a*b
+ans4=a/b
+
+print("addition is",ans1)
+print("subtraction is",ans2)
+print("multiplication is",ans3)
+print("division is",ans4)
