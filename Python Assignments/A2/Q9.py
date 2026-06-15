5d9819b834ef999769ae17fc2f0184acbc42cba4
omnaravane <omnaravane@gmail.com>
2026-06-15 18:41:17 +0530

    Create Q9.py

---
+++ Python Assignments/A2/Q9.py
@@ -0,0 +1,10 @@
+#program to show name and age+1
+print("enter your name")
+name=input()
+
+print("enter your age")
+age=int(input())
+
+age=age+1
+
+print("Hello",name,"you will turn",age,"next year.")
